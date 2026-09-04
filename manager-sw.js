f749ef5a0b32af3b86c2c49e0688cb6e503eae5d
hasmushtaq158-hub <hasmushtaq158@gmail.com>
2026-09-05 00:51:37 +0400

    Refresh manager app cache after dashboard redesign

--- manager-sw.js
+++ manager-sw.js
@@ -1,4 +1,4 @@
-const CACHE="park-manager-v2";
+const CACHE="park-manager-v3";
 const CORE=["./","./index.html","./manager-manifest.webmanifest","./manager-icon.svg","./DejaVuSans.ttf"];
 self.addEventListener("install",event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
